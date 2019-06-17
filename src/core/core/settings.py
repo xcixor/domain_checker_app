09e115da45b6f801aa53460f3e5c88c2141169a5
xcixor <pndungu54@gmail.com>
2019-06-17 16:04:35 +0300

    fix(configurations): change heroku and linting settings

--- src/core/core/settings.py
+++ src/core/core/settings.py
@@ -119,10 +119,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_ROOT = 'core/assets'
-MEDIA_URL = '/assets/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
